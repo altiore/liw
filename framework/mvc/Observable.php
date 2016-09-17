9e0eaabdcbef04ca7b3057ef02d8273ebee13637
Razzwan <razvanlomov@gmail.com>
2016-09-17 15:54:10 +0300

    Observer realization. Observer test

---
+++ framework/mvc/Observable.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: r
+ * Date: 17.09.16
+ * Time: 15:17
+ */
+
+namespace liw\mvc;
+
+
+interface Observable
+{
+    /**
+     * @param Observer $observer
+     *
+     * @return mixed
+     */
+    public function addObserver(Observer $observer);
+
+    /**
+     * @param Observer $observer
+     *
+     * @return mixed
+     */
+    public function removeObserver(Observer $observer);
+
+    /**
+     * Execute methods Observer::handleEvent for all observers
+     *
+     * @return void
+     */
+    public function notifyObservers();
+}
---
+++ tests/mvc/ControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace tests\mvc;
+
+use liw\mvc\Controller;
+use liw\mvc\View;
+use liw\Request;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Created by PhpStorm.
+ * User: r
+ * Date: 17.09.16
+ * Time: 15:34
+ */
+class ControllerTest extends TestCase
+{
+    public function testObserverPattern()
+    {
+        $controller = new Controller();
+        $controller->addObserver(new View(new Request()));
+        $this->expectOutputString(Controller::class);
+        $controller->notifyObservers();
+    }
+
+}
